7953ebd96fd8589b0ee2f40889db46b3e863b467
victorsoares1990-art <victor.soares1990@gmail.com>
2026-09-22 09:54:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -24,5 +24,5 @@ def historico(ticker):
         "mensagem": "Endpoint histórico funcionando"
     })
 
-if _name_ == "__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
